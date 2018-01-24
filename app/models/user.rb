9ff15f645c48c7a9e5b85b798a5fd6c1c8acacb8
Ajay <ajay@joshsoftware.com>
2018-01-24 10:33:13 +0530

    annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id            :integer          not null, primary key
+#  name          :string
+#  gender        :string
+#  email         :string
+#  password      :string
+#  date_of_birth :datetime
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class User < ApplicationRecord
   has_one :address
-  validates :name, :gender, :email, :password, presence: true
+  has_many :posts
+  validates :name, :gender, :email, :password, :date_of_birth, presence: true
 end
